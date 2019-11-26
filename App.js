df0601f1f9c8c1d5dc88d1fdb4cb8a92ff8e2c07
Richard Fernandez <richardfc83@gmail.com>
2019-11-26 00:08:35 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import Inputs from './inputs.js'
+import ScrollViewExample from './scroll_view.js'
+
+const App = () => {
+   return (
+      <ScrollViewExample />
+   )
+}
+export default App
